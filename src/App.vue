12b5c875b9413193698c833782bc2341c5811764
Kryštof Rykala <rykala.k@gmail.com>
2017-10-11 17:13:24 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <main-slide v-if="slideIndex == 0"/>
+    <settings v-if="slideIndex == 1"/>
+    <button v-on:click="changeSlide(0)">Main</button>
+    <button v-on:click="changeSlide(1)">Settings</button>
+  </div>
+</template>
+
+<script>
+import MainSlide from './components/MainSlide'
+import Settings from './components/Settings'
+
+export default {
+  name: 'app',
+  components: {MainSlide, Settings},
+  data () {
+    return {
+      slideIndex: 0
+    }
+  },
+
+  methods: {
+    changeSlide (index) {
+      this.slideIndex = index
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainSlide.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'MAIN SLIDE'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
